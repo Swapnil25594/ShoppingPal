959012ce82d15a6fe7c10df196b28e90d17f94ec
Swapnil25594 <33835123+Swapnil25594@users.noreply.github.com>
2018-04-04 21:28:32 +0530

    with Authentication

--- frontend/src/app/common/constants.ts
+++ frontend/src/app/common/constants.ts
@@ -2,6 +2,7 @@
 export class Constant {
     public static apiEndpoint = 'http://localhost:3000/api';
     public static loginUrl = Constant.apiEndpoint + '/Accounts/login';
+    public static signOut = Constant.apiEndpoint + '/Accounts/logout';
     public static registerUrl = Constant.apiEndpoint + '/Accounts';
     public static getUserById = Constant.apiEndpoint + '/Accounts';
     public static getCategoriesInRange = Constant.apiEndpoint + '/Categories/getCategory';
